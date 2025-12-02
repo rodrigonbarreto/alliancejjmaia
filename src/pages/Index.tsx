0aefce8a1eee776d4b7af03215248b028af6c19f
rodrinbarreto <rodrigonbarreto@gmail.com>
2025-12-02 21:21:35 +0000

    whisite

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,26 +1,6 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Programs from "@/components/Programs";
-import Methodology from "@/components/Methodology";
-import Schedule from "@/components/Schedule";
-import Gallery from "@/components/Gallery";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
-      <Header />
-      <main>
-        <Hero />
-        <Programs />
-        <Methodology />
-        <Schedule />
-        <Gallery />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <div className="min-h-screen bg-white" />
   );
 };
 
